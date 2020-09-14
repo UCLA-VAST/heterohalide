f037b79ca918d532d23beb528032b65db48c254f
Blaok <i@blaok.me>
2020-09-13 23:17:34 -0700

    add a run-all script

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,21 @@
 FROM ubuntu:bionic
-WORKDIR /usr/local/src/heterohalide
-COPY Halide Halide
-COPY new new
 
 RUN apt-get update \
   && apt-get install -y --no-install-recommends \
   build-essential \
   clang-8 \
+  git \
+  libjpeg-dev \
+  libpng-dev \
   libz-dev \
   llvm-8-dev \
   python3-pip \
   && rm -rf /var/lib/apt/lists/* \
   && python3 -m pip --no-cache install cmake
+
+WORKDIR /usr/local/src/heterohalide
+COPY Halide Halide
+COPY new new
 RUN cmake -S Halide -B build \
   && cmake --build build --parallel $(nproc) --target Halide
-RUN new/blur/run_blur.sh
+RUN new/run_all.sh
--- new/run_all.sh
+++ new/run_all.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+set -e
 base_dir="$(realpath "$(dirname "$0")")"
-halide_src="${base_dir}/../../Halide"
-build_dir="${base_dir}/../../build"
+halide_src="${base_dir}/../Halide"
+build_dir="${base_dir}/../build"
 
 # add '#define _CODEGEN_HETEROCL_GENERATE_' to the file 'Lower.cpp'
 sed -i '1i\#define _CODEGEN_HETEROCL_GENERATE_' "${halide_src}"/src/Lower.cpp
 cmake --build "${build_dir}" --target Halide
 
-g++ "${base_dir}"/blur.cpp -g -std=c++11 \
-  -I "${build_dir}"/include \
-  -I "${halide_src}"/tools \
-  -L "${build_dir}"/lib \
-  -l Halide \
-  -o "${base_dir}/blur"
-cd "${base_dir}"
-HL_DEBUG_CODEGEN=0 LD_LIBRARY_PATH="${build_dir}"/lib "${base_dir}"/blur
+for app in "${base_dir}"/*/*.cpp; do
+  g++ "${app}" -g -std=c++11 \
+    -I "${build_dir}"/include \
+    -L "${build_dir}"/lib \
+    -I "${halide_src}"/tools \
+    -l Halide \
+    -o "${app/%.cpp/}"
+  cd "$(dirname "${app}")"
+  HL_DEBUG_CODEGEN=0 LD_LIBRARY_PATH="${build_dir}"/lib "${app/%.cpp/}"
+done
 
 # delete '#define _CODEGEN_HETEROCL_GENERATE_' in the file 'Lower.cpp'
 sed -i '/#define _CODEGEN_HETEROCL_GENERATE_/d' "${halide_src}"/src/Lower.cpp
